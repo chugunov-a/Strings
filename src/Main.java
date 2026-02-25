dfd0f07be227fcb5d687efd5f5717bc126c32490
Voiaka PC <andrej.chugunov2002@yandex.ru>
2026-02-25 21:23:53 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
 
-}
+void main() {
+    System.out.println("Задание №1");
+
+    String firstName = "Ivan";
+    firstName = ' ' + firstName;
+    String middleName = "Ivanov";
+    middleName = ' ' + middleName;
+    String lastName = "Ivanovich";
+    lastName = " " + lastName;
+    String fullName = middleName + firstName + lastName;
+    System.out.println(" Ф.И.О. сотрудника — " + fullName);
+
+    System.out.println("Задание №2");
+
+    System.out.println(" Данные Ф.И.О. сотрудника для заполнения отчета — " + (fullName.toUpperCase()));
+
+    System.out.println("Задание №3");
+
+    fullName = "Иванов Семён Семёнович";
+    System.out.println(" Данные Ф.И.О. сотрудника — " + (fullName.replace("ё", "е")));
+    }
